b90d5fc46fe4dc03385516ee6682458f3d5c3327
山岡智裕 <>
2021-10-27 11:15:07 +0900

    Layoutコンポーネントの追加

--- frontend/pages/contact-page.js
+++ frontend/pages/contact-page.js
@@ -1,5 +1,24 @@
+import Layout from "../components/Layout";
+import Image from "next/images";
+
 const Contact = () => {
-  return <div>Contact Page</div>;
+  return (
+    <Layout title="Contact">
+      <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
+        <div className="mt-4">
+          <p className="font-bold">Contact Info</p>
+        </div>
+        <div className="flex justify-center mt-4">
+          <image
+            className="rounded-full"
+            src="/avatar.jpg"
+            width={60}
+            alt={60}
+          />
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Contact;
